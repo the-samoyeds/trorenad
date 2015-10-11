15c6021f8d9a1116c68358fee4b1f92cb86cdb32
Marylia Gutierrez <maryliag@gmail.com>
2015-10-11 14:01:20 -0400

    solve puzzle

--- client/templates/round/round_base.js
+++ client/templates/round/round_base.js
@@ -18,18 +18,28 @@ Template.RoundBase.events({
 });
 
 Template.RoundBase.onRendered(function(){
-  dragula([document.querySelector("#top-defaults"), document.querySelector("#bottom-defaults")]).on('drag', function (el) {
-    console.log("DRAG");
+  // dragula([document.querySelector("#top-defaults"), document.querySelector("#bottom-players")]).on('drag', function (el) {
+  //   console.log("DRAG");
+  // }).on('drop', function (el, target, source, sibling) {
+  //   console.log(el);
+  //   console.log(target);
+  //   console.log(source);
+  //   console.log(sibling);
+  //   console.log("DROP");
+  // }).on('over', function (el, container) {
+  //   console.log(container);
+  //   console.log("OVER");
+  // }).on('out', function (el, container) {
+  //   console.log("OUT");
+  // });
+
+  dragula([document.querySelector("#top-defaults"), document.querySelector("#bottom-solve")]).on('drag', function (el) {
+
   }).on('drop', function (el, target, source, sibling) {
-    console.log(el);
-    console.log(target);
-    console.log(source);
-    console.log(sibling);
-    console.log("DROP");
+
   }).on('over', function (el, container) {
-    console.log(container);
-    console.log("OVER");
+
   }).on('out', function (el, container) {
-    console.log("OUT");
+
   });
 });
