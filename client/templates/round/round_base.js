4605486bea6089ebfb5dbb622ea614dfabe7d321
Marylia Gutierrez <maryliag@gmail.com>
2015-10-11 10:39:32 -0400

    dragging cards

--- client/templates/round/round_base.js
+++ client/templates/round/round_base.js
@@ -9,3 +9,15 @@ Template.RoundBase.events({
     $('.carousel-control').toggleClass('hidden');
   }
 });
+
+Template.RoundBase.onRendered(function(){
+  dragula([document.querySelector("#top-defaults"), document.querySelector("#bottom-defaults")]).on('drag', function (el) {
+
+  }).on('drop', function (el) {
+
+  }).on('over', function (el, container) {
+
+  }).on('out', function (el, container) {
+    
+  });
+});
