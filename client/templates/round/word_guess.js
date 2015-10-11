9a039c42c42b8cc7fd59ca36dd705bd6189f4f9d
Henrique Zambon <henrique.zambon@gmail.com>
2015-10-11 14:20:01 -0400

    word guess++

---
+++ client/templates/round/word_guess.js
@@ -0,0 +1,12 @@
+Template.WordGuess.helpers({
+  word: function () {
+    return {
+      text: "THIS",
+      mode: "white"
+    };
+  },
+
+  lengthOf: function (str) {
+    return str.length;
+  }
+});
