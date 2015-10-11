d5ef5e1eed87fb5125d31f500013f6dbfad4fc8f
Luiz Aoqui <lgfa29@gmail.com>
2015-10-11 02:51:51 -0400

    fix service config

--- private/services-config.js
+++
@@ -1,9 +0,0 @@
-ServiceConfiguration.configurations.remove({
-    service: 'facebook'
-});
-
-ServiceConfiguration.configurations.insert({
-    service: 'facebook',
-    appId: '979465912116705',
-    secret: 'e116c83276075620d3c53fdd6a156f5f'
-});
