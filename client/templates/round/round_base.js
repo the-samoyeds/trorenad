9e3cf08b727b94b741db9e0738e4a69df4135071
Marylia Gutierrez <maryliag@gmail.com>
2015-10-11 11:42:11 -0400

    starting the drop

--- client/templates/round/round_base.js
+++ client/templates/round/round_base.js
@@ -12,12 +12,12 @@ Template.RoundBase.events({
 
 Template.RoundBase.onRendered(function(){
   dragula([document.querySelector("#top-defaults"), document.querySelector("#bottom-defaults")]).on('drag', function (el) {
-
+    console.log("DRAG");
   }).on('drop', function (el) {
-
+    console.log("DROP");
   }).on('over', function (el, container) {
-
+    console.log("OVER");
   }).on('out', function (el, container) {
-    
+    console.log("OUT");
   });
 });
