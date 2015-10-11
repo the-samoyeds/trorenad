662ff9f06bdd776d137a8fcc82d556ddd85f20b8
Luiz Aoqui <lgfa29@gmail.com>
2015-10-11 07:00:07 -0400

    create game flow

--- client/templates/welcome/welcome.js
+++ client/templates/welcome/welcome.js
@@ -1,4 +1,14 @@
 Template.Welcome.events({
+  'click button[name="new-game"]': function(ev) {
+    Meteor.call('addGame', function(err, _id) {
+      if (err) {
+        return console.error(err);
+      }
+
+      Router.go('game.new', { _id: _id });
+    });
+  },
+
   'click #logout': function(event) {
     Meteor.logout(function(err){
       if (err) {
