1e9ad40ef0c5edb1170ebda08e53dce53f8a7e1c
Luiz Aoqui <lgfa29@gmail.com>
2015-10-11 20:41:45 -0400

    improve layout of bottom base

--- client/templates/round/round_base.js
+++ client/templates/round/round_base.js
@@ -1,15 +1,57 @@
-Template.RoundBase.rendered = function () {
+Template.RoundBase.onRendered(function(){
+  $(function() {
+    togglePlaceholder();
+  });
+
   $('.carousel').carousel({
     interval: false
   });
-};
+
+  dragula([
+    document.querySelector("#top-defaults"),
+    document.querySelector("#bottom-solve"),
+    document.querySelector("#bottom-players"),
+  ]).on('drag', function (el) {
+
+  }).on('drop', function (el, target, source, sibling) {
+    togglePlaceholder();
+
+    var game = Games.findOne(Session.get("game")._id);
+    var logic = new WordLogic(game, true);
+
+    var code = el.innerText.trim();
+
+    var pos = 0;
+
+    $("#bottom-solve code").each(function (i, el) {
+      if (el.innerText.trim() == code) {
+        pos = i;
+      }
+    });
+
+    logic.answerWord(pos, code);
+
+  }).on('over', function (el, container) {
+    togglePlaceholder();
+  }).on('out', function (el, container) {
+    togglePlaceholder();
+  });
+
+  function togglePlaceholder() {
+    var paragraph = $('#placeholder');
+    if ($('#bottom-solve').children().length > 0) {
+      paragraph.hide();
+    } else {
+      paragraph.show();
+    }
+  }
+});
 
 Template.RoundBase.helpers({
   isMaster: function() {
     return Meteor.player().master;
   }
 });
-
 
 Template.RoundBase.events({
   'click .carousel-control': function () {
@@ -34,54 +76,4 @@ Template.RoundBase.events({
     console.log("USER ID SELECTED: " + this.userId);
   }
 
-});
-
-Template.RoundBase.onRendered(function(){
-  // dragula([document.querySelector("#top-defaults"), document.querySelector("#bottom-players")]).on('drag', function (el) {
-  //   console.log("DRAG");
-  // }).on('drop', function (el, target, source, sibling) {
-  //   console.log(el);
-  //   console.log(target);
-  //   console.log(source);
-  //   console.log(sibling);
-  //   console.log("DROP");
-  // }).on('over', function (el, container) {
-  //   console.log(container);
-  //   console.log("OVER");
-  // }).on('out', function (el, container) {
-  //   console.log("OUT");
-  // });
-
-  dragula([document.querySelector("#top-defaults"), document.querySelector("#bottom-solve")]).on('drag', function (el) {
-
-  }).on('drop', function (el, target, source, sibling) {
-
-    // TODO: Henrique
-    var paragraph = $('#bottom-solve').parent().find('p');
-    if ($('#bottom-solve').children().length > 0) {
-      paragraph.addClass('hidden');
-    } else {
-      paragraph.removeClass('hidden');
-    }
-
-    var game = Games.findOne(Session.get("game")._id);
-    var logic = new WordLogic(game, true);
-
-    var code = el.innerText.trim();
-
-    var pos = 0;
-
-    $("#bottom-solve code").each(function (i, el) {
-      if (el.innerText.trim() == code) {
-        pos = i;
-      }
-    });
-
-    logic.answerWord(pos, code);
-
-  }).on('over', function (el, container) {
-
-  }).on('out', function (el, container) {
-
-  });
 });
